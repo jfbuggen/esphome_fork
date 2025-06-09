2fe7afcf4b4424688bfe821c790cca9865712984
Basile <jf-b@outlook.be>
2025-06-09 20:34:23 +0200

    Update hbridge_valve.cpp

--- esphome/components/hbridge/valve/hbridge_valve.cpp
+++ esphome/components/hbridge/valve/hbridge_valve.cpp
@@ -16,43 +16,103 @@ void HBridgeValve::setup() {
 }
 
 void HBridgeValve::loop() {
-
+    switch(this->hbridge_state_) {
+      case HBRIDGE_SLEEP:
+        if (valve_cmd_ != VALVE_CMD_NONE) {
+          if (this->pin_sleep_ != nullptr) {
+            this->pin_sleep_->digital_write(!this->pin_sleep_->is_inverted());
+          }
+          this->hbridge_state_ = HBRIDGE_WAKEUP;
+        }
+        break
+      case HBRIDGE_IDLE:
+        if (valve_cmd_ != VALVE_CMD_NONE) {
+          // Something to do!
+        } else {
+          // Set to sleep if pin is configured
+          if (this->pin_sleep_ != nullptr) {
+            this->pin_sleep_->digital_write(this->pin_sleep_->is_inverted());
+            this->hbridge_state_ = HBRIDGE_SLEEP;
+          }
+        }
+        break;
+      case HBRIDGE_PULSE:
+        // if end of time, go to WAIT
+        break;
+      case HBRIDGE_WAIT:
+        // if end of time, go to IDLE
+        break;
+      
 )
 
 void HBridgeValve::control(const valve::ValveCall &call) {
+    if (this->valve_cmd_ != VALVE_CMD_NONE)
+    {
+        ESP_LOGV(TAG, "New command reveived while previous one was not yet processed");
+    }
+  
+    // Check if a specific position is requested
     if (call.get_position().has_value()) {
-      this->position = *call.get_position();
-      this->publish_state();
-      return;
+      float position = *call.get_position();
+      if (position == valve::VALVE_OPEN) {
+          this->valve_cmd_ = VALVE_CMD_OPEN;
+      } else if (position == valve::VALVE_CLOSED) {
+          this->valve_cmd_ = VALVE_CMD_CLOSE;
+      } else {
+          ESP_LOGW(TAG, "Ignoring request to set valve to position %f (not supported)", position);
+          this->valve_cmd_ = VALVE_CMD_NONE;
+      }
+    // Check if toggle is requested
     } else if (call.get_toggle().has_value()) {
       if (call.get_toggle().value()) {
-        if (this->position == valve::VALVE_OPEN) {
-          this->position = valve::VALVE_CLOSED;
-          this->publish_state();
-        } else {
-          this->position = valve::VALVE_OPEN;
-          this->publish_state();
-        }
+          this->valve_cmd_ = VALVE_CMD_TOGGLE;
       }
-      return;
+    // Check if stop is requested
     } else if (call.get_stop()) {
-      this->current_operation = valve::VALVE_OPERATION_IDLE;
-      this->publish_state();  // Keep the current position
-      return;
+      ESP_LOGW(TAG, "Ignoring request to stop valve (not supported)");
+      this->valve_cmd_ = VALVE_CMD_NONE;
     }
+  if (this->valve_cmd_ != VALVE_CMD_NONE) {
+    this->execute_();
+  }
 }
 
-void HBridgeValve::write_state(bool state) {
-  this->desired_state_ = state;
-  if (!this->timer_running_)
-    this->timer_fn_();
-}
-
-void HBridgeValve::timer_fn_() {
+void HBridgeValve::execute_() {
   uint32_t next_timeout = 0;
 
-  while ((uint8_t) this->desired_state_ != this->valve_state_) {
-    switch (this->valve_state_) {
+  // If timer is running, we are either during a pulse, or waiting after last pulse
+  if (timer_running_) {
+    return;
+  }
+  
+  while (this->valve_cmd_ != VALVE_CMD_NONE) {
+    command_ = this->valve_cmd_;
+    // Convert toggle command into either open or close
+    if (command_ == VALVE_CMD_TOGGLE)
+    {
+      switch (this->valve_state_) {
+        case VALVE_STATE_OPEN:
+        case VALVE_STATE_UNKNOWN:    // Toggling from unknown will request a close
+          command_ = VALVE_CMD_CLOSE;
+          break;
+        case VALVE_STATE_CLOSE:
+          command_ = VALVE_CMD_OPEN;
+          break
+        default:
+          command_ = VALVE_CMD_NONE;
+      }
+    }
+    if ((this->valve_state_ == VALVE_STATE_OPEN) && (command_ == VALVE_CMD_CLOSE)) {
+          timer_running = true;
+          this->pin_a_->digital_write(true);
+          this->pin_b_->digital_write(false);
+          next_timeout = this->pulse_length_;
+    } else if ((this->valve_state_ == VALVE_STATE_CLOSED) && (command_ == VALVE_CMD_OPEN)) {
+          timer_running = true;
+          this->pin_a_->digital_write(false);
+          this->pin_b_->digital_write(true);
+          next_timeout = this->pulse_length_;
+    }
       case VALVE_STATE_ON:
       case VALVE_STATE_OFF:
       case VALVE_STATE_UNKNOWN:
