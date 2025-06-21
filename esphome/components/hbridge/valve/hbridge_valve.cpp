85ccbfc20c1a1459d2dc9d54eb5598edd3a610e1
Basile <jf-b@outlook.be>
2025-06-21 18:57:30 +0200

    Update hbridge_valve.cpp

--- esphome/components/hbridge/valve/hbridge_valve.cpp
+++ esphome/components/hbridge/valve/hbridge_valve.cpp
@@ -53,7 +53,7 @@ void HBridgeValve::loop() {
 		if (this->valve_cmd_ != VALVE_CMD_NONE) {
 		  // Execute command
 		  bool open_ = (this->valve_cmd_ == VALVE_CMD_OPEN);
-      ESP_LOGD(TAG,"Command %s"open_ ? "OPEN" : "CLOSE");
+      ESP_LOGD(TAG,"Command %s", open_ ? "OPEN" : "CLOSE");
 		  this->hbridge_pulse_start_(open_);
 		  this->current_operation = (open_ ? VALVE_OPERATION_OPENING : VALVE_OPERATION_CLOSING);
 		  if (this->optimistic_) {
